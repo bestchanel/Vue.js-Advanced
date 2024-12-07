0234b2725cc6c2efddb5d612d36412d9fefd5867
bestchanel <samsunga77789@gmail.com>
2024-12-07 20:49:30 +0700

    Additional_Learning_EP_3

--- vue-vite-antdesign-ts-pinia/vite.config.ts
+++ vue-vite-antdesign-ts-pinia/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from "@intlify/vite-plugin-vue-i18n"
 import { fileURLToPath, URL } from 'url'
-import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +9,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  plugins: [
-    vue(),
-    vueI18n({
-      include: resolve(__dirname, "./locales/**"),
-      runtimeOnly: false
-    }),
-  ]
+  plugins: [vue()]
 })
