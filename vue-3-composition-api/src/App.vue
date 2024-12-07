e6e96c30608523151a1759ea2d8d3a19645f81fc
bestchanel <samsunga77789@gmail.com>
2024-12-07 20:28:41 +0700

    Learning_Day_2

---
+++ vue-3-composition-api/src/App.vue
@@ -0,0 +1,86 @@
+
+
+
+
+<template>
+  <div>
+<!-- <h1>{{ title }}</h1>
+<h1>{{ message }}</h1>
+<h1>{{ year }}</h1> -->
+<Home-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  // import { ref } from "vue"
+import HomeView from "./components/HomeView.vue";
+
+    //  const title = ref("hello world!")
+    //  const message = ref("hello world! yahoo") 
+    //  const year = ref(2022) 
+
+    //  setTimeout(() => {
+    //   title.value = 'hello haha'
+    // }, 2000);
+
+</script>
+
+<!-- <script lang="ts">
+import { defineComponent, ref, reactive } from 'vue'
+
+export default defineComponent({ -->
+
+    // Composition API
+  // setup () {
+    
+    //REF รองรับ PREMITIVE TYPR STRING, NUMBER, BOOLEAN INCLUDING OBJECTS
+    // const title = ref("hello world!") 
+    // const message = ref("hello world! yahoo") 
+    // const year = ref("hello world! year") 
+    // console.log(title);  
+
+    //reactive รองรับเฉพาะ object เท่านั้น!!!!!!!!!!!!!
+    // const data = ref({
+    //   title: 'Hello na',
+    //   message: "Welcome message",
+    //   year: 2022
+
+    
+    // })
+    // console.log(data);
+
+    // setTimeout(() => {
+      // title.value = "Hello gg"
+    //   data.value.title = 'hello haha'
+    // }, 2000);
+
+  //   // return {title, message, year}
+  //   return {data}
+  // },
+
+  // data(){
+  //   return{
+  //     product: "test"
+  //   }
+  // }
+   
+  // // option API
+  //  data(){
+  //   return{
+  //     title: "Hellow World"
+  //   }
+  //  },
+
+  //  mounted(){
+  //   setTimeout(() =>{
+  //     this.title = "Hellow fluke"
+  //   },2000)
+  //  }
+
+<!-- })
+</script> -->
+
+<style scoped>
+
+</style>
---
+++ vue-3-composition-api/src/components/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <button @click="counter++">Counter Up</button>
+    <h3>{{ counter }}</h3>
+    <button @click="counter--">Counter Down</button>
+    <h3>{{ person.name }} {{ person.age }}</h3>
+    <button @click="updateperson">Update Person</button>
+    <h3>{{ brand }}</h3>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+import { onMounted, reactive, ref } from "vue";
+const counter = ref(0)
+
+const person = reactive({ name: 'Woraprat', age: 22 })
+
+const updateperson = () => {
+  person.name = 'Best',
+    person.age = 22
+}
+
+onMounted(()=>{
+  setTimeout(() => {
+    person.name = 'Best'
+  }, 2000);
+})
+
+
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      brand: "Pook"
+    }
+  },
+}
+
+</script>
+
+<style scoped></style>
